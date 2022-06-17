c4df87400ff2878480ee880585b07c705cef8465
Ruben <21150133+Roob19@users.noreply.github.com>
2022-06-17 12:54:06 -0700

    remedied SignUpForm issue

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,6 +1,6 @@
+import { useState } from "react";
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import LoginForm from '../../components/LoginForm/LoginForm';
-import {useState} from "react";
 
 export default function AuthPage({setUser}) {
     const [showLogin, setShowLogin] = useState(true);
@@ -8,11 +8,12 @@ export default function AuthPage({setUser}) {
     return (
         <main>
             <h1>AuthPage</h1>
+            <button onClick={() => setShowLogin(!showLogin)}>{ showLogin ? 'Sign Up' : 'Log In' }</button>
+            <hr></hr>
             { showLogin 
                 ? <LoginForm setUser={setUser} /> 
                 : <SignUpForm setUser={setUser} /> 
             }
-            <button onClick={() => setShowLogin(!showLogin)}>{ showLogin ? 'Sign Up' : 'Log In' }</button>
         </main>
     );
 }
