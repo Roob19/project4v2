17c56364d4d0b73eb570eb0a772701628a5fafad
Ruben <21150133+Roob19@users.noreply.github.com>
2022-06-17 09:13:57 -0700

    not giving up

--- src/App.js
+++ src/App.js
@@ -1,70 +1,24 @@
 import React from "react";
-import axios from 'axios';
 import "./App.css";
 
-const YELP_BASE_URL = 'https://api.yelp.com/v3/';
-const YELP_BIZ_URL = 'https://api.yelp.com/v3/businesses/search';
-const YELP_EVT_URL = 'https://api.yelp.com/v3/events';
 
-const YELP_TOKEN = process.env.REACT_APP_YELP_TOKEN;
 
-class App extends React.Component {
-  // Constructor
-  constructor(props) {
-    super(props);
+const App = () => {
+  
+  
 
-    this.state = {
-      items: [],
-      DataisLoaded: false,
-    };
-  }
-
-  // ComponentDidMount is used to
-  // execute the code
-  componentDidMount() {
-    axios.get(`https://cryptic-headland-94862.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=95742`, 
-    {
-      method: 'GET',
-      // mode: "no-cors",
-      headers: {
-        Authorization: `Bearer ${YELP_TOKEN}`, 
-        // Origin: 'localhost',
-        withCredentials: true, 
-      }, 
-      // params: {
-      //   location: '95742', 
-      //   categories: 'beergardens',
-      // }
-    })
-      .then((res) => res.json())
-      .then((json) => {
-        this.setState({
-          items: json,
-          DataisLoaded: true,
-        });
-      });
-  }
-  render() {
-    const { DataisLoaded, items } = this.state;
-    if (!DataisLoaded)
-      return (
-        <div>
-          <h1> Pleses wait some time.... </h1>{" "}
-        </div>
-      );
-
-    return (
-      <div className="App">
-        <h1> Fetch data from an api in react </h1>{" "}
-        {items.map((item) => (
-          <ol key={item.id}>
-            User_Name: {item.name}, Full_Name: {item.alias}, User_Email:{" "}
-            {item.location.display_address}
-          </ol>
-        ))}
-      </div>
-    );
-  }
+//     return (
+//       <div className="App">
+//         <h1> Fetch data from an api in react </h1>{" "}
+//         {items.map((item) => (
+//           <ol key={item.id}>
+//             User_Name: {item.name}, Full_Name: {item.alias}, User_Email:{" "}
+//             {item.location.display_address}
+//           </ol>
+//         ))}
+//       </div>
+//     );
+//   }
 }
 
 export default App;
---
+++ src/utilities/services/getYelp.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import axios from 'axios';
+
+const YELP_TOKEN = process.env.REACT_APP_YELP_TOKEN;
+
+const YELP_BASE_URL = 'https://api.yelp.com/v3/';
+const YELP_BIZ_URL = 'https://api.yelp.com/v3/businesses/search';
+const YELP_EVT_URL = 'https://api.yelp.com/v3/events';
+
+
+const getYelp = () => {
+    const prep = {
+    mode: "no-cors",
+    headers: {
+        Autorization: `Bearer ${YELP_TOKEN}`,
+        "Access-Control-Allow-Origin": "http://localhost:3000",
+        "Access-Control-Allow-Credentials": true,
+    },
+    params: {
+        location: "95742",
+        radius: 39999,
+        categories: "beergardens",
+        sort_by: "rating",
+        limit: 10,
+    },
+    };
+
+    axios
+        .get(`${YELP_BIZ_URL}`, prep)
+        .then((res) => {
+            console.log(res);
+            // console.log(res.data.businesses[0].name);
+        });
+};
+
+export {
+    getYelp,
+}
