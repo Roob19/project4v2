bf3eaccde7f076e9acc25d0b4afc57494194306b
Ruben <21150133+Roob19@users.noreply.github.com>
2022-06-17 10:16:15 -0700

    moving App to /pages and stubbing it up

---
+++ src/pages/App/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import {useState} from "react";
+import AuthPage from "../AuthPage/AuthPage";
+import {Route, Switch, Redirect} from "react-router-dom";
+import { getUser } from '../../utilities/users-service';
+import NavBar from "../../components/NavBar/NavBar";
+
+export default function App() {
+    const [user, setUser] = useState(getUser());
+    return (<main className="App">
+            {user ? <>
+                <NavBar user={user} setUser={setUser}/>
+                <Switch>
+                    <Route path="/BeerBars">
+                        {/* <BeerBarsPage /> */}
+                    </Route>
+                    <Route path="/BeerGardens">
+                        {/* <BeerGardensPage /> */}
+                    </Route>
+                    <Route path="/BeerTours">
+                        {/* <BeerToursPage /> */}
+                    </Route>
+                    <Route path="/events">
+                        {/* <eventsPage /> */}
+                    </Route>
+                    <Redirect to="/"/>
+                </Switch>
+            </> : <AuthPage setUser={setUser}/>}
+        </main>);
+}
