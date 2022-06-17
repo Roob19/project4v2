9629052e27814bdd6fdfa45e2cba1e2856f94ad6
Ruben <21150133+Roob19@users.noreply.github.com>
2022-06-16 22:30:45 -0700

    prototype

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const YELP_BASE_URL = 'https://api.yelp.com/v3/';
+const YELP_BIZ_URL = 'https://api.yelp.com/v3/businesses/search';
+const YELP_EVT_URL = 'https://api.yelp.com/v3/events';
+
+const YELP_TOKEN = process.env.REACT_APP_YELP_TOKEN;
+
+class App extends React.Component {
+  // Constructor
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+      DataisLoaded: false,
+    };
+  }
+
+  // ComponentDidMount is used to
+  // execute the code
+  componentDidMount() {
+    fetch(`https://api.yelp.com/v3/businesses/search?location=95742`, 
+    {
+      method: 'GET',
+      mode: "no-cors",
+      headers: {
+        Authorization: `Bearer ${YELP_TOKEN}`, 
+        Origin: 'localhost',
+        withCredentials: true, 
+      }, 
+      // params: {
+      //   location: '95742', 
+      //   categories: 'beergardens',
+      // }
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          items: json,
+          DataisLoaded: true,
+        });
+      });
+  }
+  render() {
+    const { DataisLoaded, items } = this.state;
+    if (!DataisLoaded)
+      return (
+        <div>
+          <h1> Pleses wait some time.... </h1>{" "}
+        </div>
+      );
+
+    return (
+      <div className="App">
+        <h1> Fetch data from an api in react </h1>{" "}
+        {items.map((item) => (
+          <ol key={item.id}>
+            User_Name: {item.name}, Full_Name: {item.alias}, User_Email:{" "}
+            {item.location.display_address}
+          </ol>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/utilities/seeds/beerTours.js
@@ -0,0 +1,218 @@
+export const beerToursData = [
+  {
+    id: "-dANAEth6XwSxSOMNoJuqA",
+    alias: "sac-brew-bike-sacramento-3",
+    name: "Sac Brew Bike",
+    image_url:
+      "https://s3-media2.fl.yelpcdn.com/bphoto/7PmLgoeeCWo6aMQbaqvYdA/o.jpg",
+    is_closed: false,
+    url: "https://www.yelp.com/biz/sac-brew-bike-sacramento-3?adjust_creative=BRwnxxQVDh4lnO7BFVYRrQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=BRwnxxQVDh4lnO7BFVYRrQ",
+    review_count: 290,
+    categories: [
+      {
+        alias: "partybikerentals",
+        title: "Party Bike Rentals",
+      },
+      {
+        alias: "beertours",
+        title: "Beer Tours",
+      },
+      {
+        alias: "beerbar",
+        title: "Beer Bar",
+      },
+    ],
+    rating: 5.0,
+    coordinates: {
+      latitude: 38.5708179,
+      longitude: -121.4837016,
+    },
+    transactions: [],
+    price: "$$",
+    location: {
+      address1: "1519 19th St",
+      address2: null,
+      address3: "",
+      city: "Sacramento",
+      zip_code: "95811",
+      country: "US",
+      state: "CA",
+      display_address: ["1519 19th St", "Sacramento, CA 95811"],
+    },
+    phone: "+19169527973",
+    display_phone: "(916) 952-7973",
+    distance: 23965.454632211808,
+  },
+  {
+    id: "8vNjE8hgMTq6fOZ4sc8lDQ",
+    alias: "sactown-social-bikes-sacramento",
+    name: "Sactown Social Bikes",
+    image_url:
+      "https://s3-media1.fl.yelpcdn.com/bphoto/cVGqmwZ8BUDZ70Ip_ivtLg/o.jpg",
+    is_closed: false,
+    url: "https://www.yelp.com/biz/sactown-social-bikes-sacramento?adjust_creative=BRwnxxQVDh4lnO7BFVYRrQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=BRwnxxQVDh4lnO7BFVYRrQ",
+    review_count: 49,
+    categories: [
+      {
+        alias: "bikerentals",
+        title: "Bike Rentals",
+      },
+      {
+        alias: "partybikerentals",
+        title: "Party Bike Rentals",
+      },
+      {
+        alias: "beertours",
+        title: "Beer Tours",
+      },
+    ],
+    rating: 5.0,
+    coordinates: {
+      latitude: 38.57402,
+      longitude: -121.4944,
+    },
+    transactions: [],
+    location: {
+      address1: "1100 O St",
+      address2: "Ste D",
+      address3: null,
+      city: "Sacramento",
+      zip_code: "95814",
+      country: "US",
+      state: "CA",
+      display_address: ["1100 O St", "Ste D", "Sacramento, CA 95814"],
+    },
+    phone: "+19169682453",
+    display_phone: "(916) 968-2453",
+    distance: 24867.735700579447,
+  },
+  {
+    id: "Z2G8WH82uFwd_QSpw8jsIg",
+    alias: "sacramento-river-cruise-clarksburg-2",
+    name: "Sacramento River Cruise",
+    image_url:
+      "https://s3-media2.fl.yelpcdn.com/bphoto/-6kh-1n4Q9a7gAG_HiGqLg/o.jpg",
+    is_closed: false,
+    url: "https://www.yelp.com/biz/sacramento-river-cruise-clarksburg-2?adjust_creative=BRwnxxQVDh4lnO7BFVYRrQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=BRwnxxQVDh4lnO7BFVYRrQ",
+    review_count: 37,
+    categories: [
+      {
+        alias: "winetours",
+        title: "Wine Tours",
+      },
+      {
+        alias: "beertours",
+        title: "Beer Tours",
+      },
+      {
+        alias: "boattours",
+        title: "Boat Tours",
+      },
+    ],
+    rating: 5.0,
+    coordinates: {
+      latitude: 38.418891,
+      longitude: -121.526071,
+    },
+    transactions: [],
+    location: {
+      address1: "36339 Riverview Dr",
+      address2: "",
+      address3: null,
+      city: "Clarksburg",
+      zip_code: "95612",
+      country: "US",
+      state: "CA",
+      display_address: ["36339 Riverview Dr", "Clarksburg, CA 95612"],
+    },
+    phone: "+19168007887",
+    display_phone: "(916) 800-7887",
+    distance: 32510.91397645443,
+  },
+  {
+    id: "0DXeeKi58FnTTvnrU1d95Q",
+    alias: "bohemian-highway-travel-sonoma-3",
+    name: "Bohemian Highway Travel",
+    image_url:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/sMBhQkRGmYLpwWJd-TofbQ/o.jpg",
+    is_closed: false,
+    url: "https://www.yelp.com/biz/bohemian-highway-travel-sonoma-3?adjust_creative=BRwnxxQVDh4lnO7BFVYRrQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=BRwnxxQVDh4lnO7BFVYRrQ",
+    review_count: 5,
+    categories: [
+      {
+        alias: "foodtours",
+        title: "Food Tours",
+      },
+      {
+        alias: "winetours",
+        title: "Wine Tours",
+      },
+      {
+        alias: "beertours",
+        title: "Beer Tours",
+      },
+    ],
+    rating: 5.0,
+    coordinates: {
+      latitude: 38.28691,
+      longitude: -122.4655,
+    },
+    transactions: [],
+    location: {
+      address1: "",
+      address2: null,
+      address3: "",
+      city: "Sonoma",
+      zip_code: "95476",
+      country: "US",
+      state: "CA",
+      display_address: ["Sonoma, CA 95476"],
+    },
+    phone: "+17072049660",
+    display_phone: "(707) 204-9660",
+    distance: 113255.61202229481,
+  },
+  {
+    id: "7IlewmMteSzYuhiJQzzwHQ",
+    alias: "sonoma-wine-rides-santa-rosa-2",
+    name: "Sonoma Wine Rides",
+    image_url:
+      "https://s3-media2.fl.yelpcdn.com/bphoto/3KiAJgWI1QVVWqIupVYO-Q/o.jpg",
+    is_closed: false,
+    url: "https://www.yelp.com/biz/sonoma-wine-rides-santa-rosa-2?adjust_creative=BRwnxxQVDh4lnO7BFVYRrQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=BRwnxxQVDh4lnO7BFVYRrQ",
+    review_count: 9,
+    categories: [
+      {
+        alias: "winetours",
+        title: "Wine Tours",
+      },
+      {
+        alias: "beertours",
+        title: "Beer Tours",
+      },
+      {
+        alias: "bustours",
+        title: "Bus Tours",
+      },
+    ],
+    rating: 5.0,
+    coordinates: {
+      latitude: 38.41305,
+      longitude: -122.74573,
+    },
+    transactions: [],
+    location: {
+      address1: "2820 Coleman Glen Ln",
+      address2: "",
+      address3: "",
+      city: "Santa Rosa",
+      zip_code: "95407",
+      country: "US",
+      state: "CA",
+      display_address: ["2820 Coleman Glen Ln", "Santa Rosa, CA 95407"],
+    },
+    phone: "+17073281408",
+    display_phone: "(707) 328-1408",
+    distance: 134993.2619762306,
+  },
+];
