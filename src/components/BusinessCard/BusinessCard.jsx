14e86056329704b20164907a5193445d5d43791d
Ruben <21150133+Roob19@users.noreply.github.com>
2022-06-24 10:22:40 -0700

    mongo_uri

--- src/components/BusinessCard/BusinessCard.jsx
+++ src/components/BusinessCard/BusinessCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const BusinessCard = ({business, idx}) => {
+const BusinessCard = ({business}) => {
     return (
         <div className="business-card" key={business.id}>
             <Link to={`/business/${business.id}`}>
