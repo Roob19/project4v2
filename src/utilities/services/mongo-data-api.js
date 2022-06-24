4f03c201606617c52c10566e878595201fe72856
Ruben <21150133+Roob19@users.noreply.github.com>
2022-06-24 11:07:45 -0700

    troubleshooting

---
+++ src/utilities/services/mongo-data-api.js
@@ -0,0 +1,28 @@
+var axios = require('axios');
+var data = JSON.stringify({
+    "collection": "<COLLECTION_NAME>",
+    "database": "<DATABASE_NAME>",
+    "dataSource": "project2Cluster",
+    "projection": {
+        "_id": 1
+    }
+});
+            
+var config = {
+    method: 'post',
+    url: 'https://data.mongodb-api.com/app/data-utrdn/endpoint/data/v1/action/findOne',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Headers': '*',
+      'api-key': process.env.REACT_APP_MONGO_DATA_API_KEY,
+    },
+    data: data
+};
+            
+axios(config)
+    .then(function (response) {
+        console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
