a2889168ad399c4c83a9a8fb8eb7883fc4578923
Ruben <21150133+Roob19@users.noreply.github.com>
2022-06-24 09:28:34 -0700

    figuring out businesscard display

--- src/pages/BusinessIndex/BusinessIndex.jsx
+++ src/pages/BusinessIndex/BusinessIndex.jsx
@@ -1,10 +1,10 @@
 import { BusinessCard } from "../../components/BusinessCard/BusinessCard";
 
-const BusinessIndex = (props) => {
+const BusinessIndex = ({ business }) => {
     return (
         <div>
             <div className="businesses-container">
-                {props.map((biz, idx) => {
+                {business.map((biz, idx) => {
                     return (
                         <BusinessCard business={biz} idx={idx} />
                     )
